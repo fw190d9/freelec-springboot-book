4cb78bd83bb6d0308ed82923876f44fbbe1ea1a7
fw190d9 <fw190@hanmail.net>
2020-04-21 02:12:52 +0900

    ignore file created

--- src/main/java/springboot/web/IndexController.java
+++ src/main/java/springboot/web/IndexController.java
@@ -5,22 +5,28 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import springboot.config.auth.LoginUser;
+import springboot.config.auth.dto.SessionUser;
 import springboot.service.PostsService;
 import springboot.web.dto.PostsResponseDto;
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
 
         model.addAttribute("posts", postsService.findAllDesc());
-        List temp = postsService.findAllDesc();
-        System.out.println("TEMP:" + temp);
+
+        if(user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
